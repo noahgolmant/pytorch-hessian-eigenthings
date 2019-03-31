4a2bcf27c24eebd08d6103b12a61968881f2be69
Noah Golmant <noah.golmant@gmail.com>
2019-03-31 11:33:00 -0700

    add find_packages in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 """setup.py for hessian_eigenthings"""
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 install_requires = [
     'numpy>=0.14',
     'torch>=0.4'
 ]
 
-setup(name="hessian_eigenthings", author="Noah Golmant", install_requires=install_requires)
+setup(name="hessian_eigenthings",
+      author="Noah Golmant",
+      install_requires=install_requires,
+      packages=find_packages(),
+      description='Eigendecomposition of model Hessians in PyTorch!')
